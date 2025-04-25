a4d91c7f2bdebd6302a2245ae515562be6bc2b0a
Haoran <150745856+HaoranLMaoMao@users.noreply.github.com>
2025-04-25 23:51:56 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="match-series-batch",
     version="0.1.0",
-    author="Haoran",
+    author="Haoran Ma",
     author_email="haoran.ma@ikz-berlin.de",
     description="Batch non-rigid image alignment using pymatchseries.",
     long_description=open('README.md').read(),
